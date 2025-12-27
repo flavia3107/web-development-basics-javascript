a015300f72f0486469defc8cd2997979425f5fb2
Flavia Kosti <flaviakosti@gmail.com>
2025-12-27 13:16:45 -0500

    Re-design

--- exercise.js
+++ exercise.js
@@ -1,10 +1,7 @@
 const firstButtonElement = document.querySelector('button');
 const secondButtonElement = document.getElementById('change-bg-btn');
-
-const firstParagraphElement = document.body.children[3].children[1];
-console.log(document.body.children);
+const firstParagraphElement = document.body.children[1].children[2]?.children[1];
 const thirdParagraphElement = firstParagraphElement?.nextElementSibling?.nextElementSibling;
-console.log(thirdParagraphElement);
 
 function removeParagraph() {
   thirdParagraphElement.remove();
